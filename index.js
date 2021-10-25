b88ea31ea71ac969fcdb09a58fb21830db6e0931
Max <max.edison@yahoo.com>
2021-10-25 17:06:38 -0400

    combined new-joke feature and tally-counter feature onto one button (eventual goal of finished app)

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-let marks = document.createElement("div");
-document.body.appendChild(marks);
-let fives = document.createElement("span");
-fives.classList.add("line-thru");
-marks.appendChild(fives);
-let ones = document.createElement("span");
-ones.classList.add("red");
-marks.appendChild(ones);
-let testSubject = 0;
+let marks = document.getElementById("marks");
+let fives = document.getElementById("fives");
+let ones = document.getElementById("ones");
+
+
+let featJoke = document.createElement("div");
+document.body.appendChild(featJoke);
+let featSetup = document.createElement("p");
+featJoke.appendChild(featSetup);
+let featPunchline = document.createElement("p");
+featJoke.appendChild(featPunchline);
+
+let punchButton = document.getElementById("punch-button");
+let jokeStr = "";
 
 const likeButton = document.getElementById("like-button");
 
-likeButton.addEventListener("click", event =>{
+let totalVal = 0;
 
-testSubject += 1;
-console.log(testSubject);
-let quotient = Math.floor(testSubject/5);
-console.log(quotient);
-let remainder = (testSubject%5);
-console.log(remainder);
-let tally = ""
-let tallies = ""
+likeButton.addEventListener("click", event =>{
+    totalVal+=1;
+    tallyMark(totalVal);
+    newJoke();
+})
+
+function tallyMark(count){
+    console.log(count);
+    let quotient = Math.floor(count/5);
+    console.log(quotient);
+    let remainder = (count%5);
+    console.log(remainder);
+    let tally = ""
+    let tallies = ""
 
 for(let i=0; i<quotient; i++){
     fives.textContent="";
@@ -33,34 +44,18 @@ for(let k=0; k<remainder; k++){
     // let tally = "";
     tally += "|"
     ones.append(tally);
-}
-})
+}}
 
-let featJoke = document.createElement("div");
-document.body.appendChild(featJoke);
-let featSetup = document.createElement("p");
-featJoke.appendChild(featSetup);
-let featPunchline = document.createElement("p");
-featJoke.appendChild(featPunchline);
-
-let punchButton = document.getElementById("punch-button");
-let jokeObj = {};
-let jokeStr = "";
-
-
-let newButton = document.getElementById("new-button");
-newButton.addEventListener("click", event =>{
-
+function newJoke(){
     fetch("https://icanhazdadjoke.com",{headers: {"Content-Type":"application/json", "Accept":"application/json"}})
     .then(resp => resp.json())
     .then(data => {
         punchButton.classList.add("hide-me");
         featPunchline.textContent = "";
         console.log(data.joke)
-        jokeObj = data;
         jokeStr = data.joke;    
     if (data.joke.includes("?")){
-        let parsedJoke = data.joke.split("?", 2);
+        let parsedJoke = data.joke.split("?");
         featSetup.textContent=`${parsedJoke[0]}?`
         jokeStr = `${parsedJoke[1]}`
         punchButton.classList.remove("hide-me");
@@ -68,8 +63,7 @@ newButton.addEventListener("click", event =>{
     else{
         featSetup.textContent = jokeStr;
     }})
-
-})
+}
 
 
 
@@ -77,10 +71,3 @@ punchButton.addEventListener("click", (event)=>{
    featPunchline.textContent = jokeStr;
    punchButton.classList.add("hide-me");
 })
-
-
-    // featPunchline.textContent = `${parsedJoke[1]}`
-    // jokeStr = parsedJoke[1];
-    // console.log(jokeStr);
-
-    
